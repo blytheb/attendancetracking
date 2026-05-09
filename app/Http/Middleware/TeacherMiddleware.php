e1ddb9cdf838448fc0db9a553040e1fd47d29fc0
blytheb <blytheballesteros@gmail.com>
2026-05-08 14:30:03 -1000

    created admin and teacher roles and middleware

---
+++ app/Http/Middleware/TeacherMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class TeacherMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::user() && Auth::user()->role != 'teacher') {
+            return redirect()->route('admin.dashboard');
+        }
+
+        return $next($request);
+    }
+}
