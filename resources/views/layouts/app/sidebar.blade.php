ca5abb2b2b6d5fef0e572656ea4961a255810019
blytheb <blytheballesteros@gmail.com>
2026-05-11 08:40:16 -1000

    created admin and teacher dashboard with middleware auth

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -6,13 +6,27 @@
     <body class="min-h-screen bg-white dark:bg-zinc-800">
         <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.header>
-                <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
+                <x-app-logo
+                    :sidebar="true"
+                    :href="auth()->user()->role === 'admin'
+                    ? route('admin.dashboard')
+                    : route('teacher.dashboard')"
+                    wire:navigate
+                />
+
                 <flux:sidebar.collapse class="lg:hidden" />
             </flux:sidebar.header>
 
             <flux:sidebar.nav>
                 <flux:sidebar.group :heading="__('Platform')" class="grid">
-                    <flux:sidebar.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
+                    <flux:sidebar.item
+                    icon="home"
+                    :href="auth()->user()->role === 'admin'
+                        ? route('admin.dashboard')
+                        : route('teacher.dashboard')"
+                    :current="request()->routeIs('admin.dashboard') || request()->routeIs('teacher.dashboard')"
+                    wire:navigate
+                    >
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
                 </flux:sidebar.group>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Livewire\Admin\AdminDashboard;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::view('dashboard', 'dashboard')->name('dashboard');
+Route::middleware(['teacher', 'auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('teacher.dashboard');
+});
+
+Route::middleware(['admin', 'auth', 'verified'])->group(function () {
+    Route::get('/admin/dashboard', AdminDashboard::class)->name('admin.dashboard');
 });
 
 require __DIR__.'/settings.php';
